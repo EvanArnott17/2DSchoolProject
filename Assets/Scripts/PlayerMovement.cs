7b8ec8567b58533873eb076c1f15e77fe0002667
Evan Arnott <evanarnott17@gmail.com>
2026-01-24 15:03:59 -0700

    Added gravity to objects

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,23 +3,61 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody2D body;
+    private Vector2 _moveDirection;
+
+    [SerializeField] private float speed = 5f;
+
+    [SerializeField] private Rigidbody2D body;
+
+    private SpriteRenderer playerSprite;
+
+    private InputActions _playerInput;
+
+
+    private void Awake()
+    {
+        _playerInput = new InputActions();
+    }
+
+    private void OnEnable()
+    {
+        _playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerInput.Disable();
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello World!");   
+        playerSprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        body.position += new Vector2(1, 0) * Time.deltaTime;
+        _moveDirection = _playerInput.Player.Move.ReadValue<Vector2>();
+
+        _moveDirection = new Vector2(_moveDirection.x, _moveDirection.y);
+
+        ChangePlayerDirection(_moveDirection);
+
+        body.linearVelocity = _moveDirection * speed;
     }
 
-    private void OnMove(InputAction.callbackcontext context)
+    private void ChangePlayerDirection(Vector2 direction)
     {
-        
+        if (_moveDirection.x < 0)
+        {
+
+            playerSprite.flipX = true;
+        }
+        else if (_moveDirection.x > 0)
+        {
+            playerSprite.flipX = false;
+        }
     }
 
 
